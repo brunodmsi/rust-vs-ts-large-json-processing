4b0bb7d1e432957e41bd5eb323f24bc2d27044f5
Bruno De Masi <brunodemasi1@gmail.com>
2023-03-21 22:24:01 -0300

    feat(rust): adding struct and learning how to loop over it while making it safe

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,13 +1,34 @@
+#![allow(non_snake_case)]
+
 use std::fs::File;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct DataResult {
+    id: String,
+    isActive: bool,
+    registered: String,
+    tags: Vec<String> 
+}
 
 fn main() -> std::io::Result<()> {
     let file = File::open("../giant-json.json")
         .expect("File should open");
 
-    let json: serde_json::Value = serde_json::from_reader(file)
+    let data_result: Vec<DataResult> = serde_json::from_reader(file)
         .expect("File should be proper JSON");
 
-    println!("{}", json["is_giant"]);
+    let mut ids = Vec::<&String>::new();
+
+    for result in data_result.iter() {
+        if !result.isActive {
+            continue;
+        }
+
+        ids.push(&result.id);
+    }
+
+    println!("{:?}", ids);
 
     Ok(())
 }
